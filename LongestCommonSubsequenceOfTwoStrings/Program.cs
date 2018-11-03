0cd6c566495e47a225f8b12e8c7ee73c5103d23c
gibeulah <gibeulah@microsoft.com>
2018-11-04 01:34:02 +0530

    FirstCommit

--- LongestCommonSubsequenceOfTwoStrings/Program.cs
+++ LongestCommonSubsequenceOfTwoStrings/Program.cs
@@ -6,7 +6,34 @@ namespace LongestCommonSubsequenceOfTwoStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Longest Common Subsequence!");
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine("Enter the first string");
+            string firstString = Console.ReadLine();
+            Console.WriteLine("Enter the second string");
+            string secondString = Console.ReadLine();
+
+            int[,] lcs = new int[firstString.Length+1, secondString.Length+1];
+
+            for (int i = 0; i <= firstString.Length; i++) {
+                for (int j = 0; j <= secondString.Length; j++) {
+                    if (i == 0 || j == 0) {
+                        lcs[i, j] = 0;
+                    }
+                    else if (firstString[i - 1] == secondString[j - 1])
+                    {
+                        lcs[i, j] = 1 + lcs[i - 1, j - 1];
+                    }
+                    else {
+                        lcs[i, j] = Math.Max(lcs[i-1, j], lcs[i, j-1]);
+                    }
+                }
+            }
+
+            Console.WriteLine("The length of common subsequence is "+lcs[firstString.Length, secondString.Length]);
+
+            Console.ReadLine();
         }
     }
 }
